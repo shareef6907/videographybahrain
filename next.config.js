52b708e5c16c9f52ae0ce94866219c18436f214c
Shareef <events.photos.videos@gmail.com>
2025-04-15 18:33:18 +0300

    Add Next.js configuration and package.json

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [], // Add any image domains you'll use here
+  },
+}
+
+module.exports = nextConfig 
